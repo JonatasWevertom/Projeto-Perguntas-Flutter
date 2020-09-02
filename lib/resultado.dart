3064b23d22e7d2955753fe59cc41145cc8fc5e87
JonatasWevertom <jonataswevertom@hotmail.com>
2020-09-02 18:50:28 -0300

    first commit

---
+++ lib/resultado.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns!';
+    } else if (pontuacao < 12) {
+      return 'Você é bom!';
+    } else if (pontuacao < 16) {
+      return 'impressionante';
+    } else {
+      return 'Nivel Jedi';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 28),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        FlatButton(
+          child: Text(
+            'Reiniciar?',
+            style: TextStyle(fontSize: 18),
+          ),
+          textColor: Colors.blue,
+          onPressed: quandoReiniciarQuestionario,
+        )
+      ],
+    );
+  }
+}
